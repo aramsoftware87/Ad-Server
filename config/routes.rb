c5b6185d717bd352be725a420e0069fbc453721f
Gerlando Piro <gerlando@gmail.com>
2010-07-31 18:18:53 -0700

    updated layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   
-  map.root :controller => 'session'
+  map.root :controller => 'admin', :action => 'login'
   
   
   map.resources :websiteplacements
